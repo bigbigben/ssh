c52eefcd1f2166db09cbff8dd505eabf261cfd2f
mldn <yanghaifeng886@126.com>
2015-11-03 12:02:26 +0800

    Change hello.java in master branch

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class Hello{
 	public static void main(String args[]){
 	     System.out.println("hello world ");
+		 Admin admin = new Admin();
+		 Dept dept = new Dept();
+		 System.out.println("hello java");
 	}
 }
